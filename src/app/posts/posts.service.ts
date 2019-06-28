70e4f27a957b3a83b1bcd0fda8565fe1507c5c77
valentinaa <tinamontserrat@hotmail.com>
2019-06-28 16:55:08 -0300

    posts service

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { Post } from './post';
@@ -14,7 +14,26 @@ export class PostsService {
     return this.http.get<Post[]>(this.urlBase)
     .pipe(catchError(this.tratarErro));
   }
+  adicionarPosts(umPost: Post): Observable<Post>{
+  const opcoesHttp = { 
+    headers: new HttpHeaders({
+      'Content-type': 'application/json'
+    })
+    }; 
+    return this.http.post<Post>(this.urlBase, umPost, opcoesHttp).pipe(catchError(this.tartadorErros<Post>()));
 
+  }
+  private tartadorErros<T>(resultado?:T){
+    return (erro: HttpErrorResponse): Observable<T> => {
+      if (erro.error instanceof ErrorEvent){
+        console.log(`Erro: ${erro.error.message}`);
+        return throwError(`Erro: ${erro.error.message}`);
+      }else{
+        console.log(`Status: ${erro.status}`);
+        return of (resultado as T);
+      }
+    };
+  }
   private tratarErro(erro: HttpErrorResponse) {
     if(erro.error instanceof ErrorEvent){
       console.log(`Erro : ${erro.error.message}`);
